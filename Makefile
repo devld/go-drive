2592433ddc9f3ba79db355e5ac0c59fd731176d0
devld <devlduio@gmail.com>
2020-12-04 18:13:36 +0800

    Github Actions integration

--- Makefile
+++ Makefile
@@ -4,19 +4,28 @@ target_name = go-drive_$(GOOS)_$(GOARCH)
 build_dir = $(work_dir)/$(target_name)
 
 all: $(build_dir)/$(target_name).tar.gz
+zip: $(build_dir)/$(target_name).zip
 
+# tar.gz
 $(build_dir)/$(target_name).tar.gz: $(build_dir)/go-drive $(build_dir)/web $(build_dir)/lang
 	cd $(work_dir); tar acf $(target_name).tar.gz --owner=0 --group=0 $(target_name)
 
-$(build_dir)/go-drive: $(build_dir)
-	go build -o $(build_dir)
+# zip for windows
+$(build_dir)/$(target_name).zip: $(build_dir)/go-drive $(build_dir)/web $(build_dir)/lang
+	cd $(work_dir); zip -q -r $(target_name).zip $(target_name)
 
-$(build_dir)/web: $(build_dir)
-	cd web; npm install && npm run build
+$(build_dir)/go-drive: $(build_dir)
+	go build -o $(build_dir) -ldflags \
+		"-X go-drive/common.version=${BUILD_VERSION} -X go-drive/common.hash=$(shell git rev-parse HEAD) -X go-drive/common.build=$(shell date +'%Y%m%d')"
+
+$(build_dir)/web: $(build_dir) web/dist
 	cp -R web/dist $(build_dir)/web
 
 $(build_dir)/lang: $(build_dir)
 	cp -R docs/lang $(build_dir)/
+
+web/dist:
+	cd web; npm install && npm run build
 
 $(build_dir): check-env
 	mkdir -p $(build_dir)
--- storage/db.go
+++ storage/db.go
@@ -9,6 +9,14 @@ import (
 	"go-drive/common/utils"
 )
 
+var initSQL = []string{
+	"INSERT INTO users(username, password) VALUES ('admin', '$2y$10$Xqn8qV2D2KY2ceI5esM/JOiKTPKJFbkSzzuhce89BxygvCqnhyk3m')", // 123456
+	"INSERT INTO groups(name) VALUES ('admin')",
+	"INSERT INTO user_groups(username, group_name) VALUES ('admin', 'admin')",
+	"INSERT INTO path_permissions(path, subject, permission, policy, depth) VALUES ('', 'ANY', 1, 1, 0)",
+	"INSERT INTO path_permissions(path, subject, permission, policy, depth) VALUES ('', 'g:admin', 3, 1, 0)",
+}
+
 func NewDB(config common.Config, ch *registry.ComponentsHolder) (*DB, error) {
 	dialect, args := config.GetDB()
 
@@ -35,9 +43,32 @@ func NewDB(config common.Config, ch *registry.ComponentsHolder) (*DB, error) {
 		return nil, e
 	}
 
+	if e := tryInitDbData(db); e != nil {
+		_ = db.Close()
+		return nil, e
+	}
+
 	d := &DB{db: db}
 	ch.Add("db", d)
 	return d, nil
+}
+
+func tryInitDbData(db *gorm.DB) error {
+	n := 0
+	if e := db.Model(&types.User{}).Count(&n).Error; e != nil {
+		return e
+	}
+	if n > 0 {
+		return nil
+	}
+	return db.Transaction(func(tx *gorm.DB) error {
+		for _, initSQL := range initSQL {
+			if e := tx.Exec(initSQL).Error; e != nil {
+				return e
+			}
+		}
+		return nil
+	})
 }
 
 type DB struct {
