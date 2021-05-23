e577655d44640c9a5bbd9f2f4bc51dbc83bf365a
devld <devlduio@gmail.com>
2021-05-23 12:31:34 +0800

    fix: first startup error

--- storage/db.go
+++ storage/db.go
@@ -16,8 +16,8 @@ var initSQL = []string{
 	"INSERT INTO users(username, password) VALUES ('admin', '$2y$10$Xqn8qV2D2KY2ceI5esM/JOiKTPKJFbkSzzuhce89BxygvCqnhyk3m')", // 123456
 	"INSERT INTO groups(name) VALUES ('admin')",
 	"INSERT INTO user_groups(username, group_name) VALUES ('admin', 'admin')",
-	"INSERT INTO path_permissions(path, subject, permission, policy, depth) VALUES ('', 'ANY', 1, 1, 0)",
-	"INSERT INTO path_permissions(path, subject, permission, policy, depth) VALUES ('', 'g:admin', 3, 1, 0)",
+	"INSERT INTO path_permissions(path, subject, permission, policy) VALUES ('', 'ANY', 1, 1)",
+	"INSERT INTO path_permissions(path, subject, permission, policy) VALUES ('', 'g:admin', 3, 1)",
 }
 
 func NewDB(config common.Config, ch *registry.ComponentsHolder) (*DB, error) {
