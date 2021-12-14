9eac90e9aede5d2d80fb4b8e0a9e670cf85a0d9a
devld <devlduio@gmail.com>
2021-12-14 09:47:55 +0800

    feat: PDF preview(browser)

---
+++ web/src/utils/handlers/pdf.js
@@ -0,0 +1,17 @@
+import { T } from '@/i18n'
+import PdfView from '@/views/HandlerViews/PdfView.vue'
+import { filenameExt } from '@/utils'
+
+export default {
+  name: 'pdf',
+  display: {
+    name: T('handler.pdf.name'),
+    description: T('handler.pdf.desc'),
+    icon: '#icon-wendang'
+  },
+  view: {
+    name: 'PdfView',
+    component: PdfView
+  },
+  supports: entry => entry.type === 'file' && filenameExt(entry.name) === 'pdf'
+}
