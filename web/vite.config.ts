612fe28386ece9a03b9ae808b4e16b78b6baa178
devld <devlduio@gmail.com>
2022-08-23 10:03:30 +0000

    fix: dir thumbnail icon style

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
     port: 9803,
     proxy: {
       '/api': {
-        target: 'http://localhost:8099/go-drive/',
+        target: 'http://localhost:8089',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
